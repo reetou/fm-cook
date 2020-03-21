de43b8f94e08f49cd661ed2a7b499aa7c8afcff9
reetou <fillmasterson@gmail.com>
2020-03-22 02:05:16 +0300

    init commit

---
+++ App.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { AsyncStorage } from 'react-native';
+import OnboardingView from "./views/Onboarding";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Some info will be here</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ textAlign: 'center' }}>Заказов пока нет. Пройдите сертификацию и все будет.</Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+const TABS = {
+  PROFILE: 'Профиль',
+  ORDERS: 'Заказы'
+}
+
+export default function App() {
+  const [showOnboarding, setShowOnboarding] = useState<boolean>(false)
+  useEffect(() => {
+    const checkInitialSettings = async () => {
+      try {
+        const passedOnboarding: null | string = await AsyncStorage.getItem('passed_onboarding')
+        if (!passedOnboarding || passedOnboarding !== 'true') {
+          setShowOnboarding(true)
+        }
+      } catch (e) {
+        console.error('Error happened', e)
+      }
+    }
+    checkInitialSettings()
+  }, [])
+  return (
+    <React.Fragment>
+      {
+        showOnboarding
+          ? (
+            <OnboardingView
+              onDone={async () => {
+                await AsyncStorage.setItem('passed_onboarding', 'true')
+                setShowOnboarding(false)
+              }}
+            />
+          )
+          : (
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === TABS.PROFILE) {
+                      iconName = focused
+                        ? 'ios-information-circle'
+                        : 'ios-information-circle-outline';
+                    } else if (route.name === TABS.ORDERS) {
+                      iconName = focused ? 'ios-list-box' : 'ios-list';
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                  },
+                })}
+                tabBarOptions={{
+                  activeTintColor: 'tomato',
+                  inactiveTintColor: 'gray',
+                }}
+              >
+                <Tab.Screen name={TABS.PROFILE} component={HomeScreen} />
+                <Tab.Screen name={TABS.ORDERS} component={SettingsScreen} />
+              </Tab.Navigator>
+            </NavigationContainer>
+          )
+      }
+    </React.Fragment>
+  );
+}
---
+++ views/Onboarding.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Image, View, Text, StyleSheet, AsyncStorage } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import { Ionicons } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+  mainContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  image: {
+    width: 320,
+    height: 320,
+  },
+  text: {
+    color: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 22,
+    color: 'white',
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+});
+
+export interface OnboardingViewProps {
+  onDone: () => void;
+}
+
+export default function OnboardingView(props: OnboardingViewProps) {
+  return (
+    <AppIntroSlider
+      bottomButton
+      onDone={props.onDone}
+      renderItem={({item, dimensions}) => (
+        <LinearGradient
+          style={[
+            styles.mainContent,
+            dimensions,
+          ]}
+          colors={item.colors}
+          start={{ x: 0, y: 0.1 }}
+          end={{ x: 0.1, y: 1 }}
+        >
+          <Ionicons
+            style={{ backgroundColor: 'transparent' }}
+            name={item.icon}
+            size={200}
+            color="white"
+          />
+          <View>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.text}>{item.text}</Text>
+          </View>
+        </LinearGradient>
+      )}
+      slides={[
+        {
+          key: 'somethun',
+          title: 'Сделано для вас',
+          text: 'Можно забрать еду самому или заказать доставку',
+          icon: 'md-copy',
+          colors: ['#63E2FF', '#B066FE'],
+        },
+        {
+          key: 'somethun1',
+          title: 'Приемлемая цена',
+          text: 'Оплата напрямую и без комиссии - повара продают без наценки',
+          icon: 'ios-add',
+          colors: ['#A3A1FF', '#3A3897'],
+        },
+        {
+          key: 'somethun2',
+          title: 'Безопасно',
+          text: 'Тщательно проверяем поваров',
+          icon: 'md-done-all',
+          colors: ['#29ABE2', '#4F00BC'],
+        },
+      ]}
+    />
+  );
+}
