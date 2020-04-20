2cf6305d1c8e2497b088c800395bbeb0f68b05b2
reetou <fillmasterson@gmail.com>
2020-04-20 12:33:10 +0300

    Add disabled state for action button

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -3,6 +3,7 @@ import { TouchableOpacity, Text, View } from "react-native";
 import Styleguide from "../Styleguide";
 import { StatusColorType } from "../utils";
 import { hexToRgb } from "../animatedUtils";
+import TouchableScale from 'react-native-touchable-scale';
 
 interface Props {
   text: string;
@@ -13,13 +14,16 @@ interface Props {
 
 export default function ActionButton(props: Props) {
   const rgb = hexToRgb(Styleguide.getColorByType(props.color))
+  const disabledRgb = hexToRgb(Styleguide.tintColor)
   return (
     <View style={{ paddingHorizontal: 6, flex: 1 }}>
-      <TouchableOpacity
+      <TouchableScale
+        pressInFriction={10}
+        pressOutFriction={10}
         disabled={props.disabled}
         style={{
           paddingVertical: 16,
-          backgroundColor: `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.2)`,
+          backgroundColor: props.disabled ? `rgba(${disabledRgb.r}, ${disabledRgb.g}, ${disabledRgb.b}, 0.2)` : `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.2)`,
           height: 52,
           borderRadius: 26,
         }}
@@ -30,12 +34,12 @@ export default function ActionButton(props: Props) {
             fontWeight: '600',
             fontSize: 15,
             textAlign: 'center',
-            color: Styleguide.getColorByType(props.color)
+            color: props.disabled ? Styleguide.tintColor : Styleguide.getColorByType(props.color)
           }}
         >
           {props.text}
         </Text>
-      </TouchableOpacity>
+      </TouchableScale>
     </View>
   )
 }
