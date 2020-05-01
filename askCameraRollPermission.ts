332b4c778c84eaab86e6f1433d149897ee564523
reetou <fillmasterson@gmail.com>
2020-05-01 21:07:09 +0300

    Fix permissions ask for android

--- askCameraRollPermission.ts
+++ askCameraRollPermission.ts
@@ -2,10 +2,10 @@ import * as Permissions from 'expo-permissions';
 import { Alert } from "react-native";
 
 export default async function askCameraRollPermission(): Promise<boolean> {
-  const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+  const data = await Permissions.askAsync(Permissions.CAMERA_ROLL);
 
-  if (status !== 'granted') {
-    Alert.alert('Ошибка', 'Не получено разрешение на доступ к галерее')
+  if (data.status === 'denied') {
+    Alert.alert('Ошибка', `Не получено разрешение на доступ к галерее`)
     return false;
   }
 
